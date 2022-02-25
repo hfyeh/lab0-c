0403a99c5c96bb1e057a22fbc93f168560a6ddb2
Hsueh-Fang Yeh <sharefun010407@gmail.com>
2022-02-25 13:41:03 +0800

    Implement insert_tail, remove_tail, and delete_mid

--- queue.c
+++ queue.c
@@ -67,6 +67,26 @@ bool q_insert_head(struct list_head *head, char *s)
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+
+    size_t len = strlen(s);
+
+    element->value = malloc(strlen(s) + 1);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+
+    strncpy(element->value, s, len);
+    element->value[len] = '\0';
+
+    list_add_tail(&element->list, head);
+
     return true;
 }
 
@@ -91,7 +111,19 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || head->next == head)
+        return NULL;
+
+    struct list_head *node = head->prev;
+    list_del_init(node);
+
+    element_t *element = container_of(node, element_t, list);
+    if (sp) {
+        strncpy(sp, element->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+
+    return element;
 }
 
 /* Return number of elements in queue */
@@ -111,6 +143,20 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+    if (!head || head->next == head)
+        return false;
+
+    struct list_head *mid = head->next;
+    for (struct list_head *fast = head->next->next;
+         fast != head && fast->next != head; fast = fast->next->next) {
+        mid = mid->next;
+    }
+    list_del(mid);
+
+    element_t *element = container_of(mid, element_t, list);
+    q_release_element(element);
+
     return true;
 }
 
